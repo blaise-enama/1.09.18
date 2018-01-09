9619c261fd5c159f68c4bc87b661dbf29315c650
blaise-enama <34690993+blaise-enama@users.noreply.github.com>
2018-01-09 14:46:38 -0500

    Add files via upload

---
+++ 1.09.18 Booleans .py
@@ -0,0 +1,10 @@
+print("Welcome to Gmail")
+myUsername =input("Enter your username, then press Return: ")
+
+if myUsername=="phyllisbrown": #== because you are comparing two values
+    print("Username Successful")
+    myPassword =input("Enter your password, then press Return: ")
+    if myPassword=="sof1230":
+        print("Success")
+    else: print("Incorrect Password")
+else: ("Incorrect username, try again") 
